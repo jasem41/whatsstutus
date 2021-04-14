11976ea0b7caf4a5833b7ff61a57cb01b4254181
jasem41 <shardnet.tk@gmail.com>
2021-04-14 23:22:14 +0300

    Add files via upload

--- ads.php
+++ ads.php
@@ -2,7 +2,7 @@
 
 
 	$set = array('erorr'=>'false',
-	              'app_id' => 'ca-app-pub-3940256099942544/3419835294',
+	              'app_id' => 'ca-app-pub-3940256099942544~334751171',
 				  'bannar_id' => 'ca-app-pub-3940256099942544/6300978111',
 				  'intersturial_id'=>'ca-app-pub-3940256099942544/1033173712',
 				  'native_id'=>'ca-app-pub-3940256099942544/2247696110',
@@ -10,7 +10,8 @@
 				  'tital'=>'اشحن شدات بوبجي',
 				  'img'=>'https://play-lh.googleusercontent.com/JRd05pyBH41qjgsJuWduRJpDeZG0Hnb0yjf2nWqO7VaGKL10-G5UIygxED-WNOc3pg',
 				  'dsc'=>'اشحن شدات بوبجي الان',
-				  'link'=>'https://www.shardnat.tech');
+				  'link'=>'https://www.shardnat.tech',
+				  'version'=>'3');
 
  header( 'Content-Type: application/json; charset=utf-8' );
         			 $json = json_encode($set);	    
